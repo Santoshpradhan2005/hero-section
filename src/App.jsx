7cb9d5537ca082557c4c0de5291ce82159e0c0f1
santosh pradhan <saank980@gmail.com>
2026-01-02 18:03:44 +0530

    first component design

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Hero from "./Hero";
+
+const App = () => {
+  return (
+    <div className="flex h-screen justify-center bg-neutral-900">
+      <Hero />
+      
+    </div>
+  );
+};
+
+export default App;
+
+
